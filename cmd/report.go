26588d54f08aaa564914982047b7dc5dc2a89e70
Keran Yang <yangkr920208@gmail.com>
2024-05-22 13:54:02 -0400

    generate a report snapshot url
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- cmd/report.go
+++ cmd/report.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"bytes"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -25,12 +27,16 @@ var reportCmd = &cobra.Command{
 		// Fetch the dashboard
 		dashboardData, err := fetchDashboard(grafanaURL, auth, dashboardID)
 		if err != nil {
-			log.Error("Error fetching dashboard.")
 			return err
 		}
-		log.Info("Successfully fetched the dashboard.")
-		fmt.Println(string(dashboardData[:]))
-		fmt.Println("dummy-url")
+
+		// Create a snapshot
+		reportUrl, err := createSnapshot(grafanaURL, auth, dashboardData)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(reportUrl)
 		return nil
 	},
 }
@@ -45,13 +51,40 @@ func fetchDashboard(grafanaURL, auth, dashboardID string) ([]byte, error) {
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
-
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
 	return ioutil.ReadAll(resp.Body)
+}
+
+func createSnapshot(grafanaURL, auth string, dashboardData []byte) (string, error) {
+	snapshotURL := grafanaURL + "/api/snapshots"
+	req, _ := http.NewRequest("POST", snapshotURL, bytes.NewBuffer(dashboardData))
+	req.Header.Add("Authorization", "Basic "+auth)
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	var result struct {
+		URL string `json:"url"`
+	}
+	if err := json.Unmarshal(body, &result); err != nil {
+		return "", fmt.Errorf("error parsing JSON response: %v", err)
+	}
+	if result.URL == "" {
+		return "", fmt.Errorf("snapshot URL not found in response")
+	}
+	return result.URL, nil
 }
 
 func init() {
