54a20838b352fdd62375f690a08f0bc7e857124e
Keran Yang <yangkr920208@gmail.com>
2024-05-23 12:22:12 -0400

    feat: create datasource and connect to dashboard
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- report/dashboard.go
+++ report/dashboard.go
@@ -93,3 +93,99 @@ func CreateSnapshot(grafanaURL, auth string, dashboardData []byte) (string, erro
 	}
 	return result.URL, nil
 }
+
+// CreateGrafanaDataSource assumes there is a prometheus server running on http://localhost:9090
+// and creates a new Grafana data source called Numaflow-PerfMan-Prometheus connecting to prometheus.
+// It returns the uid of the created data source.
+func CreateGrafanaDataSource(grafanaURL, auth string) (string, error) {
+	dataSource := map[string]interface{}{
+		"name":      "Numaflow-PerfMan-Prometheus",
+		"type":      "prometheus",
+		"url":       "http://localhost:9090",
+		"access":    "proxy",
+		"isDefault": false,
+	}
+
+	data, err := json.Marshal(dataSource)
+	if err != nil {
+		return "", err
+	}
+
+	dataSourceURL := grafanaURL + "/api/datasources"
+	req, _ := http.NewRequest("POST", dataSourceURL, bytes.NewBuffer(data))
+	req.Header.Add("Authorization", "Basic "+auth)
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode >= 300 {
+		return "", fmt.Errorf("failed to fetch data sources: %s", string(body))
+	}
+
+	result := struct {
+		Datasource struct {
+			UID string `json:"uid"`
+		} `json:"datasource"`
+		Message string `json:"message"`
+	}{}
+
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return "", fmt.Errorf("failed to unmarshal JSON response: %s", err)
+	}
+	return result.Datasource.UID, nil
+}
+
+// GrafanaDatasource for parsing datasource details
+type GrafanaDatasource struct {
+	UID  string `json:"uid"`
+	Name string `json:"name"`
+}
+
+// FetchGrafanaDataSourceUID fetches the UID of an existing data source, given the name.
+func FetchGrafanaDataSourceUID(grafanaURL, auth string) (string, error) {
+	dataSourceURL := grafanaURL + "/api/datasources"
+	req, _ := http.NewRequest("GET", dataSourceURL, nil)
+	req.Header.Add("Authorization", "Basic "+auth)
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode >= 300 {
+		return "", fmt.Errorf("failed to create data source: %s", string(body))
+	}
+
+	var dataSources []GrafanaDatasource
+	err = json.Unmarshal(body, &dataSources)
+	if err != nil {
+		return "", fmt.Errorf("failed to unmarshal JSON response: %s", err)
+	}
+
+	for _, ds := range dataSources {
+		if ds.Name == "Numaflow-PerfMan-Prometheus" {
+			return ds.UID, nil
+		}
+	}
+
+	return "", fmt.Errorf("data source not found: %s", "Numaflow-PerfMan-Prometheus")
+}
