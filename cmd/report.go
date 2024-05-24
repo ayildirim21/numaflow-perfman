a5f5fd0af06945eccee8e70e4b43e4d4efce361e
Keran Yang <yangkr920208@gmail.com>
2024-05-24 11:56:53 -0400

    .
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- cmd/report.go
+++ cmd/report.go
@@ -51,7 +51,11 @@ var reportCmd = &cobra.Command{
 		// TODO - handle case when the dashboard already exists.
 		// (we should move data source and dashboard creation into setup and let report to only generate snapshot.)
 		resp, err := report.CreateDashboard(grafanaURL, auth, dashboardData)
+
+		fmt.Printf("dashboard id: %s", resp.UID)
+
 		if err != nil {
+			fmt.Printf("error %v\n", err)
 			return err
 		}
 
